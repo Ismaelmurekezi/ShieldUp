4eb3d1d45292aac4537d62b92665e69ce5b01f63
ismaelmurekezi <ismaelmurekezi1@gmail.com>
2025-05-09 20:35:14 +0200

    setup project and Authentication

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import connectDB from "./config/mongodb.js"
+import authRoute from "./routes/authRoute.js"
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({credentials:true}))
+
+const PORT = process.env.PORT || 5000;
+connectDB()
+
+//API Endpoints
+
+app.get('/', (req, res) => {
+    res.send("Welcome to ShieldUp Application")
+})
+app.use('/api/auth',authRoute)
+
+
+app.listen(PORT,()=>console.log(`Server is listening to port ${PORT}`))
