5a3ee7a2940fb199c0a1ed9e8a4221d3e19d864a
ismaelmurekezi <ismaelmurekezi1@gmail.com>
2025-05-29 23:33:02 +0200

    setting up frontend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div>
+      <h3 className="text-3xl text-primary font-poppin">
+        Welcome to shieldUp
+          </h3>
+          <p>Hello there</p>
+          
+    </div>
+  );
+};
+
+export default Home;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Loginss</div>
+  )
+}
+
+export default Login
